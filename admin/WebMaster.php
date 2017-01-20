0154842f2a354635f763b44d186321ff9b0c92cd
AndrewAlanGM <andrew-alan@hotmail.com>
2017-01-19 21:51:18 -0600

    Fix menu admin
    Fix a la vista del menu desktop y responsive

--- admin/WebMaster.php
+++ admin/WebMaster.php
@@ -33,18 +33,18 @@
     <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation" style="background-color:#ff544e; border-color:#ff544e;">
         <div class="container">
             <!-- Brand and toggle get grouped for better mobile display -->
-            <div class="navbar-header">
+            <div class="navbar-header" style="max-width: 100%">
                 <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
                     <span class="sr-only">Toggle navigation</span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand comic" href="WebMaster.php" style="color:#000000">INICIO</a>
+                <a class="navbar-brand comic" href="WebMaster.php" style="color:#000000;margin-left:5%">INICIO</a>
             </div>
             <!-- Collect the nav links, forms, and other content for toggling -->
-            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-                <ul class="nav navbar-nav">
+            <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1" >
+                <ul class="nav navbar-nav" style="max-width: 100%;margin-left: 3%">
                     <li>
                         <a href="WebMaster.php?modulo=usuarios" class="comic" style="color:#000000">Usuarios</a>
                     </li>   
